cc859747199c99b412cc45afcfdf9b279928dab3
jgalfaro <joaquim.garcia@gmail.com>
2023-05-02 00:17:59 +0200

    minor changes

--- matlab/oneEqQmodel.m
+++ matlab/oneEqQmodel.m
@@ -1,16 +1,25 @@
-%% *One-equation quantum model* 
+%% Quantum Shapley Value Calculation
+% *Author*: Michel Barbeau
+% 
+% Version: April 26, 2023
+
+% cd to QIT directory
+clear
+cd './qit'
+% run the init script
+init
+%% *One-equation quantum model (non-efficient, made efficient with Iain's ideas)*
 % Construct the two quantum systems (may be combined in one):
 % $$B^{\pm} (H^{\otimes n}\otimes I) \vert 0 \rangle^{\otimes n+1}$$
 % 
 % Repeatedly measure the rightmost qubit. The difference of the expected values 
-% of the systems is $\frac{\Phi_i}{V_{max}}$.
+% of the systems is \frac{\Phi_i}{V_{max}}.
 %% Quantum Version of $\gamma \left(n,m\right)$ and$V(S)$
 % Unitaries general form
 % 
 % 
-% $$B^\pm(n) = \bigoplus_{i=0}^{2^n-1} \frac{1}{V_{max}}\pmatrix{ \sqrt{1-\phi^\pm(i,n)} 
-% &   \sqrt{\phi^\pm(i,n)}  \cr\sqrt{\phi^\pm(i,n)} & -\sqrt{1-\phi^\pm(i,n)}   
-% }$$
+% $$B^\pm(n) = \bigoplus_{i=0}^{2^n-1} \pmatrix{ \sqrt{1-\phi^\pm(i,n)} &   
+% \sqrt{\phi^\pm(i,n)}  \cr\sqrt{\phi^\pm(i,n)} & -\sqrt{1-\phi^\pm(i,n)}   }$$
 % 
 % where
 %% 
@@ -21,73 +30,78 @@
 gamma = @(n,m) 1 / ( nchoosek(n,m)*(n+1) );
 c = @(i) nnz(dec2bin(i)-'0');
 %% 
-% Example assuming $n+1=3, F = {1, 2, 3}$and $i=1$
+% Example assuming $n+1=3$, $F = \{0, 1, 2\}$ and $i=1$
 
-V_0 = 0; 
-V_1 = 0;
-V_1_2  = 1;
-V_1_3  = 1;
+V_empty = 0; 
+V_0 = 0;
+V_0_1  = 1;
+V_0_2  = 1;
+V_1 = 0; 
+V_1_2 = 0;
 V_2 = 0; 
-V_2_3 = 0;
-V_3 = 0; 
-V_1_2_3 = 1;
+V_0_1_2 = 1;
 %% 
-% $V^{+}()$ and $\phi^{+}()$
+% $$W(S)$$
 
-Vp = [V_1 V_1_3 V_1_2 V_1_2_3 ];
-phip = @(i,n) gamma(n,c(i)) * Vp(i);
-%% 
-% $V^{-}()$ and $\phi^{-}()$
-
-Vm = [V_0 V_3 V_2 V_2_3 ];
-phim = @(i,n) gamma(n,c(i)) * Vm(i);
-% value function upper bound
-Vmax = max([Vp Vm]);
+W_0 = V_0 - V_empty;
+W_0_1 = V_0_1 - V_1;
+W_0_2 = V_0_2 - V_2
+W_0_1_2 = V_0_1_2 - V_1_2;
+W = [ W_0 W_0_1 W_0_2 W_0_1_2 ];
+Wmax = max(W);
+phi = @(i,n) gamma(n,c(i)) * W(i+1)/Wmax;
 n = 2;
-Bplus = 1/Vmax * [...
-    sqrt(1-phip(1,n)) sqrt(phip(1,n)) 0 0 0 0 0 0;
-    sqrt(phip(1,n)) -sqrt(1-phip(1,n)) 0 0 0 0 0 0;
-    0 0 sqrt(1-phip(2,n)) sqrt(phip(2,n)) 0 0 0 0;
-    0 0 sqrt(phip(2,n)) -sqrt(1-phip(2,n)) 0 0 0 0;
-    0 0 0 0 sqrt(1-phip(2,n))  sqrt(phip(2,n)) 0 0;
-    0 0 0 0 sqrt(phip(2,n)) -sqrt(1-phip(2,n)) 0 0;
-    0 0 0 0 0 0 sqrt(1-phip(2,n))  sqrt(phip(2,n));
-    0 0 0 0 0 0 sqrt(phip(2,n)) -sqrt(1-phip(2,n))
+B = [...
+    sqrt(1-phi(0,n)) sqrt(phi(0,n))   0 0 0 0 0 0;
+    sqrt(phi(0,n))  -sqrt(1-phi(0,n)) 0 0 0 0 0 0;
+    0 0 sqrt(1-phi(1,n)) sqrt(phi(1,n)) 0 0 0 0;
+    0 0 sqrt(phi(1,n)) -sqrt(1-phi(1,n)) 0 0 0 0;
+    0 0 0 0 sqrt(1-phi(2,n))  sqrt(phi(2,n)) 0 0;
+    0 0 0 0 sqrt(phi(2,n)) -sqrt(1-phi(2,n)) 0 0;
+    0 0 0 0 0 0 sqrt(1-phi(3,n))  sqrt(phi(3,n));
+    0 0 0 0 0 0 sqrt(phi(3,n)) -sqrt(1-phi(3,n))
     ]
 % check if unitary
-isequal(Bplus*Bplus', eye(size(Bplus*Bplus',1)))
-Bminus = 1/Vmax * [...
-    sqrt(1-phim(1,n)) sqrt(phim(1,n)) 0 0 0 0 0 0;
-    sqrt(phim(1,n)) -sqrt(1-phim(1,n)) 0 0 0 0 0 0;
-    0 0 sqrt(1-phim(2,n)) sqrt(phim(2,n)) 0 0 0 0;
-    0 0 sqrt(phim(2,n)) -sqrt(1-phim(2,n)) 0 0 0 0;
-    0 0 0 0 sqrt(1-phim(2,n))  sqrt(phim(2,n)) 0 0;
-    0 0 0 0 sqrt(phim(2,n)) -sqrt(1-phim(2,n)) 0 0;
-    0 0 0 0 0 0 sqrt(1-phim(2,n))  sqrt(phim(2,n));
-    0 0 0 0 0 0 sqrt(phim(2,n)) -sqrt(1-phim(2,n))
-    ]
-% check if unitary
-isequal(Bminus*Bminus', eye(size(Bminus*Bminus',1)))
-
+isequal(B*B', eye(size(B*B',1)))
 % create the input state
 H = gate.qft(2);
 I = gate.id(2)
 In = u_propagate(state('000'),tensor(tensor(H,H),I))
+u_propagate(In, B)
 % samples
-sp = []; sm = [];
-for k=1:50
+u = []; 
+for k=1:10000
     % apply the Shappley gate
-    Ou = u_propagate(In, Bplus);    
+    Ou = u_propagate(In, B);    
     % measure the 3rd qubit
     [~,b,~] = measure(Ou, 3);
     cbit = b - 1;
-    sp = [ sp cbit ];
-    % apply the Shappley gate
-    Ou = u_propagate(In, Bminus);    
-    % measure the 3rd qubit
-    [~,b,~] = measure(Ou, 3);
-    cbit = b - 1;
-    sm = [ sm cbit ];
+    u = [ u cbit ];
 end
-fprintf('Shapply value is: %6.1f', Vmax * (mean(sp) - mean(sm)));
-%%
+fprintf('Shapply value is: %2.4f', 2^n * Wmax * mean(u) );
+%% Quantum Algorithm for Weighted Voting Games
+% Prepartion of partition register
+% 
+% $$W_{\ell} H^{\otimes 2^{\ell}} \vert 0 \rangle^{\otimes 2^{\ell}}$$
+% 
+% $$W_\ell = \bigoplus_{i=0}^{2^n-1} \pmatrix{ \sqrt{W_{\ell}(i))} &   \sqrt{W_{\ell}(i+1)}  
+% \cr\sqrt{W_{\ell}(i+1)} & -\sqrt{W_{\ell}(i)}   }$$
+
+%partition functions
+t = @(ell,k) ( sin( (k*pi) / 2^(ell+1) ) )^2;
+w = @(ell,k) t(ell,k+1) - t(ell,k); 
+ell = 1;
+for k=0:2^ell-1
+    w(ell,k)
+end
+%% 
+% Controlled rotations
+% 
+% 
+
+
+%% Reference
+% Burge, I., Barbeau, M., & Garcia-Alfaro, J. (2023). A Quantum Algorithm for 
+% Shapley Value Estimation. _arXiv preprint arXiv:2301.04727_.
+% 
+%
